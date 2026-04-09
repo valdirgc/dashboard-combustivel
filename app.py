e2db5e7e0c8ae87d53f8ef3756d0fd2082994902
valdirgc <valdirgc01@gmail.com>
2026-04-09 12:11:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import datetime
 st.set_page_config(page_title="Sistema Frota - Jaborandi", layout="wide", initial_sidebar_state="expanded")
 
 # Inicialização limpa do Cookie Manager
-@st.cache_resource(experimental_allow_widgets=True)
+@st.cache_resource
 def get_cookie_manager():
     return stx.CookieManager()
 
